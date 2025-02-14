e33918ce4295cf240433f19f010af8d9e05e51fa
codym469 <cody206843@student-schoolsofwestfield.org>
2025-02-14 15:13:30 +0000

    age kata

--- done.js
+++ done.js
@@ -16,7 +16,13 @@
 
 
 /* Prompt */
-
+/* Var Alert Prompt */
+// make an integer variable for age, prompt user for their age
+var age = prompt("what is your age?");
+// make a string variable for name, prompt user for their name
+var name1 = prompt("what is your name?")
+// alert name is age years old
+alert(name1 + " is " + age + " years old"); 
 
 /* Prompt with Conditional */
 
